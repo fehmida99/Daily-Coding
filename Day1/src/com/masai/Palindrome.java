8ccb53597083f5021603cb0361d83a99740957a5
Your Name <fehmida1899@gmail.com>
2023-08-01 22:01:37 +0530

    day_1

--- Day1/src/com/masai/Palindrome.java
+++ Day1/src/com/masai/Palindrome.java
@@ -11,15 +11,17 @@ public class Palindrome {
     
     
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-        scanner.close();
+        String st = sc.nextLine();
+        
+        sc.close();
 
-        if (isPalindrome(input)) {
-            System.out.println("It's a palindrome!");
+        
+        if (isPalindrome(st)) {
+            System.out.println("It is a palindrome!!!");
         } else {
-            System.out.println("It's not a palindrome!");
+            System.out.println("It is not a palindrome!!!");
         }
     }
 }
