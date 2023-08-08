0ca3a08af4109a7abaa688d5505ea6771a1ab8b0
Your Name <fehmida1899@gmail.com>
2023-08-08 20:57:01 +0530

    leapear

---
+++ Day8/src/com/masai/CheckLeapYear.java
@@ -0,0 +1,23 @@
+package com.masai;
+
+public class CheckLeapYear {
+    public static void main(String[] args) {
+        int year = 2026; 
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
