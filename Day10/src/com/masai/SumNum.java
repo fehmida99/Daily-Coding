e4c94f3fe0e9159ff40c7fc9604192021ca094ce
Your Name <fehmida1899@gmail.com>
2023-08-10 21:58:15 +0530

    summnum

---
+++ Day10/src/com/masai/SumNum.java
@@ -0,0 +1,17 @@
+package com.masai;
+public class SumNum{
+    public static void main(String[] args) {
+        int number = 145; 
+        int sum = calculateSumOfDigits(number);
+        
+        System.out.println("Sum of digits: " + sum);
+    }
+    
+    public static int calculateSumOfDigits(int num) {
+        if (num == 0) {
+            return 0;
+        } else {
+            return num % 10 + calculateSumOfDigits(num / 10);
+        }
+    }
+}
