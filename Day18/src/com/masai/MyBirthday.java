aaafc0b7898a08e77f9fb69e6e569ff67496e17d
Your Name <fehmida1899@gmail.com>
2023-08-18 01:11:20 +0530

    MyBirthDay

---
+++ Day18/src/com/masai/MyBirthday.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+public class MyBirthday {
+    public static void main(String[] args) {
+        String yourName = "Fehmida"; // Replace with your name
+        int yourAge = 24; // Replace with your age
+        
+        System.out.println("Happy Birthday, " + yourName + "!");
+        System.out.println("Wishing you a fantastic day filled with joy and laughter.");
+        System.out.println("May this year be filled with exciting adventures and wonderful memories.");
+        System.out.println("On your " + yourAge + "th birthday, remember to always cherish the moments that make you smile.");
+        System.out.println("Have a wonderful day!");
+    }
+}
