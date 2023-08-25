38c06e057574bf2034f9405a92f31afd0c18a004
Your Name <fehmida1899@gmail.com>
2023-08-25 22:14:34 +0530

    gcd

---
+++ Day25/src/com/masai/GCD.java
@@ -0,0 +1,20 @@
+package com.masai;
+
+public class GCD {
+    public static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return findGCD(b, a % b);
+        }
+    }
+
+    public static void main(String[] args) {
+        int num1 = 48;
+        int num2 = 18;
+
+        int gcd = findGCD(num1, num2);
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+    }
+}
+
