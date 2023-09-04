68ac8076a8bed7c2c83a08ad589ad47a700728a2
Your Name <fehmida1899@gmail.com>
2023-09-04 21:14:46 +0530

    oops

--- OOPs_Concepts/src/com/masai/Polymorphism.java
+++ OOPs_Concepts/src/com/masai/Polymorphism.java
@@ -1,5 +1,21 @@
 package com.masai;
 
-public class Polymorphism {
+class Shape {
+    void draw() {
+        System.out.println("Drawing a shape");
+    }
+}
 
+class Circle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+class Square extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing a square");
+    }
 }
